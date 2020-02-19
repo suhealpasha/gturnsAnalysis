f31188418887d05dff2a4f53ea969c9fcf3962b6
suheal <suhalpasha@gmail.com>
2020-02-19 12:33:45 +0530

    pymongo added

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def stuff():
         convertedToString = unicodedata.normalize('NFKD', usr['scripCode']).encode('ascii','ignore')
         print(convertedToString)
         print(type(convertedToString))
-        print(nse.get_quote('AARTIDRUGS'))
+        print(nse.get_quote('tcs'))
         q = nse.get_quote(convertedToString)
         print(q)
         temp.append(q.get("lastPrice"))
